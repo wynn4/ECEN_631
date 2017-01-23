d272e6dd90147ecf4093de2fc18ebf5a2e39411a
Jesse Wynn <jesse_wynn@byu.edu>
2017-01-23 15:54:57 -0700

    added blob detector params

--- Assignment_1/task_3.py
+++ Assignment_1/task_3.py
@@ -14,10 +14,25 @@ while number <= 36:
     #then threshold it
     ret, img = cv2.threshold(abs_diff,13,255, cv2.THRESH_BINARY)
     #then use simple blob detector with size and circularity
+    #blob detector parameters
+    params = cv2.SimpleBlobDetector_Params()
+    #filter by size
+    params.filterByArea = True
+    params.minArea = 50
+    params.maxArea = 100000
+    #filter by circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.9
+    params.maxCircularity = 1
+    #create blob detector with params
+    blob_detect = cv2.SimpleBlobDetector(params)
+    keypoints = blob_detect.detect(img)
+    #draw keypoints
+    img_w_keypoints = cv2.drawKeypoints(img,keypoints,np.array([]),(0,0,255))
     #then draw the keypoints returned by simple blob detector
     #then draw the keypoints on the original frame
     #ret, img = cv2.threshold(img, 30,255, cv2.THRESH_BINARY)
-    cv2.imshow('image',img)
+    cv2.imshow('image',img_w_keypoints)
     number = number + 1
     cv2.waitKey(33)
 cv2.destroyAllWindows
--- Assignment_1/test_space.py
+++ Assignment_1/test_space.py
@@ -1,10 +1,20 @@
+# Standard imports
 import cv2
-import numpy as np
+import numpy as np;
 
+# Read image
+im = cv2.imread("blobs.jpg", cv2.IMREAD_GRAYSCALE)
 
-filename = '1.jpg'
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector()
 
-img = cv2.imread(filename,0)
-cv2.imshow('image',img)
-cv2.waitKey(5000)
-cv2.destroyAllWindows
+# Detect blobs.
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
