5e87c99081aa0e9afd0d69f72cbae811c0890ee8
Jesse Wynn <jesse_wynn@byu.edu>
2017-01-24 14:55:31 -0700

    added click-to-pixel print

--- Assignment_1/task_1.py
+++ Assignment_1/task_1.py
@@ -1,8 +1,15 @@
 import numpy as np
 import cv2
-
+refPt = []
+def click_and_print_pixel_data(event, x, y, flags, param):
+    global refPt
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x,y)]
+        print refPt
 #create a capture object
 #0 for video device 0
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', click_and_print_pixel_data)
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -12,7 +19,8 @@ while(True):
     #here we do operations on the frame (convert to grayscale here)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('wynn', gray)
+
+    cv2.imshow('image', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
